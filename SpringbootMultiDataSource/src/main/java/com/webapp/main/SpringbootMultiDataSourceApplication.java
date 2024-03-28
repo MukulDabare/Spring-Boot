5436797aa8212230fd8a2a5421cb1258304c78a4
MukulDabare <106649658+MukulDabare@users.noreply.github.com>
2024-03-28 11:13:44 +0530

    commiting

--- SpringbootMultiDataSource/src/main/java/com/webapp/main/SpringbootMultiDataSourceApplication.java
+++ SpringbootMultiDataSource/src/main/java/com/webapp/main/SpringbootMultiDataSourceApplication.java
@@ -9,6 +9,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,7 @@ public class SpringbootMultiDataSourceApplication {
 	 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMultiDataSourceApplication.class, args);
+		
 	}
 
 }
